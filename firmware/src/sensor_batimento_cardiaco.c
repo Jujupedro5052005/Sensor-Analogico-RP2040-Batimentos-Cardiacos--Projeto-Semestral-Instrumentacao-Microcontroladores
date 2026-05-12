7946ccbb3a37fdee7133bde65f46739a91fbd9b4
AbrSerafim <eabreuserafim@gmail.com>
2026-05-12 18:45:44 -0300

    Inicio leitura adc

--- firmware/src/sensor_batimento_cardiaco.c
+++ firmware/src/sensor_batimento_cardiaco.c
@@ -1,182 +1,70 @@
-/*
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/spi.h"
-#include "hardware/i2c.h"
-#include "hardware/uart.h"
+#include "pico/time.h"
+#include "hardware/adc.h"
 
-// SPI Defines
-// We are going to use SPI 0, and allocate it to the following GPIO pins
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define SPI_PORT spi0
-#define PIN_MISO 16
-#define PIN_CS   17
-#define PIN_SCK  18
-#define PIN_MOSI 19
+// Definição de pinos
+#define ADC_READ_PIN 26
 
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+// Definição de váriaveis globais
 
-// UART defines
-// By default the stdout UART is `uart0`, so we will use the second one
-#define UART_ID uart1
-#define BAUD_RATE 115200
+/*
+    De acordo com o site:https://nav.dasa.com.br/blog/frequencia-cardiaca ,
+a frequência cardiaca máxima de uma pessoa (em bpm) pode ser estimada a partir da equação
+220 - <idade>, assim será considerada como frequência de leitura 220 bpm, que convertendo
+para período em ms, utilizando T(ms)=60000/(f(bpm)), onde f(bpm) é a frequência cardiaca 
+em batimentos por minuto, que para 220 bpm, resulta em um período de 272 ms, para garantir
+margem de leitura será utilizado 200 ms.
+*/
 
-// Use pins 4 and 5 for UART1
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define UART_TX_PIN 4
-#define UART_RX_PIN 5
+/*
+    Outras fontes relevantes quanto à taxas de frequência cardíaca normal, em repouso, são
+https://www.einstein.br/n/vida-saudavel/frequencia-cardiaca-como-medir-e-o-valor-ideal-em-cada-idade
+https://www.tuasaude.com/frequencia-cardiaca/
+*/
 
+const int CALLBACK_TIME = 200;
+
+const int measures = 10; // Número de medidas para tirar a média da frequência cardíaca
+
+// Função chamada pelo timer 
+bool timer_callback(struct repeating_timer *t){
+
+    adc_run(true); // Inicia uma conversão single-shot
+    
+    return true;
+}
+
+void __isr adc_isr(void){
+    uint16_t raw = adc_fifo_get(); // Valor digital entre 0 e 4095
+    irq_clear(ADC_IRQ_FIFO);
+
+    adc_run(false); // Para a amostragem até o próximo timer
+}
 
 
 int main()
 {
     stdio_init_all();
 
-    // SPI initialisation. This example will use SPI at 1MHz.
-    spi_init(SPI_PORT, 1000*1000);
-    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
-    gpio_set_function(PIN_CS,   GPIO_FUNC_SIO);
-    gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
-    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
-    
-    // Chip select is active-low, so we'll initialise it to a driven-high state
-    gpio_set_dir(PIN_CS, GPIO_OUT);
-    gpio_put(PIN_CS, 1);
-    // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
+    // Configuração do ADC
+    adc_init(); // Habilita o ADC
+    adc_gpio_init(ADC_READ_PIN); // Habilita o pino 26
+    adc_select_input(0); // Configura porta do ADC no canal 0
 
-    // I2C Initialisation. Using it at 400Khz.
-    i2c_init(I2C_PORT, 400*1000);
-    
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+    // Configuração do FIFO
+    adc_fifo_setup(true, false, 1, false, false);
+    adc_irq_set_enabled(true);
+    irq_set_exclusive_handler(ADC_IRQ_FIFO, adc_isr);
+    irq_set_enabled(ADC_IRQ_FIFO, true);
 
-    // Set up our UART
-    uart_init(UART_ID, BAUD_RATE);
-    // Set the TX and RX pins by using the function select on the GPIO
-    // Set datasheet for more information on function select
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-    
-    // Use some the various UART functions to send out data
-    // In a default system, printf will also output via the default UART
-    
-    // Send out a string, with CR/LF conversions
-    uart_puts(UART_ID, " Hello, UART!\n");
-    
-    // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
+    // Cria um timer de repetição
+    struct repeating_timer timer;
+
+    // Cria um timer que chama a função timer_callback
+    add_repeating_timer_ms(CALLBACK_TIME, timer_callback, NULL, &timer);
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        tight_loop_contents();
     }
-}
-
-*/
-
-#include <stdio.h>
-#include "pico/stdlib.h"
-
-#define LED  25
-
-int main1() { // bem baixo nivel -> acessando diretamente os registradores
-
-    // variavel de 32 bits inteira que pode ser modificada a qualquer momento pelo codigo
-    // coloco o * para ser um ponteiro
-    // o valor é o base adress de "2.19.6.1. IO - User Bank"
-    // escolho o GPIO_25_CTRL com endereço de offset 0x0CC
-    volatile uint32_t *io_bank0_gpio_25_ctrl = (uint32_t *)0x400140CC;
-
-    // variavel com o SIO base de "2.3.1.7. List of Registers"
-    // permite habilitar como saida (diretiva de mascara) -> usamos o output enable set
-    // perceba que output enable set eh  GPIO_OE_SET = 0x024
-    // eu somo esse valor no base address 0xd0000000 dos registradores SIO_BASE
-    volatile uint32_t *sio_gpio_oe_set = (uint32_t *)(0xd0000000+0x024);
-
-    // vou usar o XOR para ficar invertendo (efeito de blink)
-    volatile uint32_t *sio_gpio_out_xor = (uint32_t *)0xD000001C;
-
-    // configurar o pino 25 com SIO (coloca 5 no FUNCSEL)
-    *io_bank0_gpio_25_ctrl = 5;
-
-    // colocar o GP25 como saida
-    // uso operação de deslocamento para direita '<<'
-    // se eu fizer o shift 25 vezes, ele para o bit 25, sem precisar calcular na mao
-    *sio_gpio_oe_set = (1 << 25);
-
-    while(1){ // LOOP
-        // mudar o estado do pino 25
-        *sio_gpio_out_xor = (1 << 25);
-
-        // gasta ciclos de clocks
-        sleep_ms(1000);
-
-        // no SDK tenho funcoes de alto nivel que abstraem ainda mais, como digitalWrite
-        // nao preciso ir no registrador
-        // documento "raspberry-pi-pico-c-sdk.pdf" 
-    }
-}
-
-int main2() { // começo a usar o struct do .h para substituir os enderecos
-
-    // variavel com o SIO base de "2.3.1.7. List of Registers"
-    // permite habilitar como saida (diretiva de mascara)
-    volatile uint32_t *sio_gpio_oe_set = (uint32_t *)0xD0000024;
-
-    // vou usar o XOR para ficar invertendo (efeito de blink)
-    volatile uint32_t *sio_gpio_out_xor = (uint32_t *)0xD000001C;
-
-    // OU: fabricante me fornece um constante que ja esta alocada no endereco 
-    // é um struct que já esta no BANK0_BASE, o endereco que copiei antes
-    // ja tem todos os endereços no struct, ai nao preciso fazer o offset a partir do endereco de base do banco 0
-    // '->' é acessar dentro do struct o io
-    // assim, nao preciso mais de *io_bank0_gpio_25_ctrl
-    io_bank0_hw->io[LED].ctrl = GPIO_FUNC_SIO;
-
-    // colocar o GP25 como saida
-    // uso operação de deslocamento para direita '<<'
-    // se eu fizer o shift 25 vezes, ele para o bit 25, sem precisar calcular na mao
-    *sio_gpio_oe_set = (1 << 25);
-
-    while(1){ // LOOP
-
-        // OU: posso usar o toggle
-        sio_hw->gpio_togl = (1 << LED);
-
-        // gasta ciclos de clocks
-        sleep_ms(4000);
-
-        // no SDK tenho funcoes de alto nivel que abstraem ainda mais, como digitalWrite
-        // nao preciso ir no registrador
-        // documento "raspberry-pi-pico-c-sdk.pdf" 
-    }
-}
-
-int main3(){ // usando SDK -> fica mais limpo de entender
-    // substitui o uso do SIO
-    gpio_init(LED);
-
-    // configurar como saida (direcao)
-    // recebe numero do IO e bool para definir entrada ou saida
-    // substitui a diretiva de mascara
-    gpio_set_dir(LED, true);
-
-    while(1){
-        // mascara XOR
-        gpio_xor_mask(1 << LED);
-
-        // delay de 200 ms
-        sleep_ms(200);
-    }
-}
-
-int main(){
-    main3();
 }
