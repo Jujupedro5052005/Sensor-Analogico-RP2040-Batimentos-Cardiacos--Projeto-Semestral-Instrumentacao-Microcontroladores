785bc4341c3bd7c4262e30491350c990f6e8ff6d
AbrSerafim <eabreuserafim@gmail.com>
2026-06-08 14:16:58 -0300

    Comentário reescritos

--- firmware/src/sensor_batimento_cardiaco.c
+++ firmware/src/sensor_batimento_cardiaco.c
@@ -10,18 +10,18 @@
 #define ADC_INPUT 0
 
 // =========================
-// GLOBAL VARIABLES
+// VARIÁVEIS GLOBAIS
 // =========================
 
 float filtered = 0.0f;
-float baseline = 0.0f;
+float baseline = 0.0f; // Baseline age como uma estimativa lenta do sinal lido
 
 bool beatDetected = false;
 
 uint32_t lastBeatTime = 0;
 
-float bpm = 0.0f;
-float smoothBpm = 0.0f;
+float bpm = 0.0f; // BPM instântaneo
+float smoothBpm = 0.0f; // BPM filtrado
 
 // =========================
 // MAIN
@@ -41,8 +41,6 @@ int main() {
 
     adc_select_input(ADC_INPUT);
 
-    sleep_ms(2000);
-
     while (true) {
 
         // =========================
@@ -51,64 +49,68 @@ int main() {
 
         uint16_t raw = adc_read();
 
-        // baseline tracking
+        // Filtro passa-baixa exponencial
         baseline = 0.99f * baseline + 0.01f * raw;
 
-        // remove DC
+        // Remoção de offset DC
         float signal = raw - baseline;
 
-        // amplify
+        // Amplificação digital de sinal
         signal *= 10.0f;
 
-        // center signal
+        // Recentralização do sinal (4096/2)
         signal += 2048.0f;
 
-        // smoothing filter
+        // Filtro exponencial a fim de suavizar o sinal
         filtered =
             0.92f * filtered +
             0.08f * signal;
 
-        // threshold
+        // Threshold; valor minimo sob qual um sinal pode ser considerado um batimento
         float threshold = 2300.0f;
 
         // =========================
-        // BEAT DETECTION
+        // DETECÇÂO DE BATIMENTO
         // =========================
 
         uint32_t now = to_ms_since_boot(get_absolute_time());
 
+        // Se o sinal pode ser um batimento e não estamos já em um pico
         if (filtered > threshold && !beatDetected) {
 
-            beatDetected = true;
+            beatDetected = true; // Marca que o sinal é um batimento
 
+            // Computa variação de tempo
             uint32_t delta = now - lastBeatTime;
 
-            // valid BPM range
+            // Intervalo válido para a frequência cardiaca 
+            // 40 < BPM < 200
             if (delta > 300 && delta < 1500) {
 
-                bpm = 60000.0f / delta;
+                bpm = 60000.0f / delta; // Conversão para min^-1
 
-                // smooth BPM
+                // Filtro exponencial para suavizar a saída do BPM
                 smoothBpm =
                     0.85f * smoothBpm +
                     0.15f * bpm;
             }
 
-            lastBeatTime = now;
+            lastBeatTime = now; // Atualiza o tempo
         }
 
-        // reset detector
+        // Reseta o detector de pico
         if (filtered < threshold - 100.0f) {
             beatDetected = false;
         }
 
         // =========================
-        // SERIAL OUTPUT
+        // SAÍDA SERIAL
         // =========================
 
-        printf("%.2f,%.2f,1000,3000\n",
+        printf("%.2f,%.2f,%.2f,1000,3000\n",
                filtered,
-               smoothBpm);
+               smoothBpm,
+               bpm);
 
         sleep_ms(10);
     }
